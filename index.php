fca403e5844480be19a574c8ead4c141e88d6f90
Altair <maciejkamat@gmail.com>
2015-08-09 23:49:53 +0200

    sth
    not much rly.

--- index.php
+++ index.php
@@ -7,17 +7,31 @@ error_reporting(E_ALL);
 <html>
 <head>
 <link href="css/dropzone.css" type="text/css" rel="stylesheet" />
-<script src="ext/jquery-2.1.4.min.js"></script>
-<script src="ext/dropzone.js"></script>
+<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/dropzone.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<div id=""></div>
+
+<div class="container-fluid">
+  <div id="dztest" class="well col-md-6" style="height: 400px"> 
+      
+  </div>
+</div>
+
+<button id="dzsub">NO DAWAJ KLIKAJ</button>
 
 <script type="text/javascript">
-	var x = $("#mydropzone").dropzone().get(0).dropzone;
-	x.processQueue();
+	var myDropzone = new Dropzone("div#dztest", { url: "upload.php"});
+
+	$("#dzsub").click(function() {
+		myDropzone.processQueue();
+	} );
 </script>
+
+
 
 
 
